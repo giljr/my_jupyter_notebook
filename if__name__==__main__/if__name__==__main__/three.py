8c563e69a741afaaabf9dd4d0ce3f940dd08aebb
J3 <giljr.2009@gmail.com>
2020-03-24 23:17:15 -0400

    How to make Packages and understand "if  __name__ == '__main__' "
    How to make Packages and understand "if  __name__ == '__main__' "

---
+++ if__name__==__main__/if__name__==__main__/three.py
@@ -0,0 +1,20 @@
+#three.pi
+#print("TOP of THREE.PY")
+
+def function1():
+	print("Function1 run just now!")
+
+def function2():
+	print("Function2 run just now!")
+
+def function3():
+	print("Function3 run just now!")
+
+
+if __name__ == '__main__':
+	# RUN THE SCRIPT!
+	# FOR CODE ORGANIZATION
+	function3()
+	function1()
+	function2()
+	
